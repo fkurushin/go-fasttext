3b4e35f5ccc3b0b4a091709bb810b756679c471c
fkurushin <f.kurushin@ya.ru>
2023-09-04 09:26:27 +0300

    save

--- fasttext.go
+++ fasttext.go
@@ -1,1 +1,15 @@
 package fasttext
+
+// #cgo LDFLAGS: -L${SRCDIR}/fastText/lib -lfasttext-wrapper -lstdc++ -lm -pthread
+
+import "C"
+
+func (idx *faissIndex) Search(x []float32, k int64) (distances []float32, labels []int64, err error) {
+	n := len(x) / idx.D()
+	distances = make([]float32, int64(n)*k)
+	labels = make([]int64, int64(n)*k)
+	if c := C.faiss_Index_search(idx.idx, C.idx_t(n), (*C.float)(&x[0]), C.idx_t(k), (*C.float)(&distances[0]), (*C.idx_t)(&labels[0])); c != 0 {
+		err = getLastError()
+	}
+	return
+}
--- fasttext/c_api/go_fasttext.hpp
+++ fasttext/c_api/go_fasttext.hpp
@@ -5,6 +5,27 @@
 extern "C" {
 #endif
 
+typedef struct {
+    char* label;
+    float prob;                                                                                         
+} go_fast_text_pair_t;
+
+int ft_load_model(const char* path);
+
+go_fast_text_pair_t* ft_predict(const char *query_in, int k, float threshold, int* result_length);
+
+int ft_get_vector_dimension();
+
+int ft_get_sentence_vector(const char* query_in, float* vector, int vector_size);
+
+int train(const char* model_name, const char* input, const char* output, int epoch, int word_ngrams, int thread, float lr);
+
+int quantize(const char* input, const char* output);
+
+int ft_save_model(const char* filename);
+
+int ft_delete();
+
 #ifdef __cplusplus
 }
 #endif
